de3df54808f8f99fef4f3facf60b7274b6ca7ec6
Coco <chuaziyifaith@gmail.com>
2022-10-19 08:36:41 +0800

    Add Predicate Parser

--- src/main/java/seedu/address/model/pet/exceptions/PetNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package seedu.address.model.pet.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified pet.
- */
-public class PetNotFoundException extends RuntimeException {
-
-}
